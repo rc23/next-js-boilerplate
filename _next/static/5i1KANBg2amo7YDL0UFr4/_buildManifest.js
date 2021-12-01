e0838e5e6a39bf2c48974be2b90f051acc6d1861
rc23 <kumipt23@gmail.com>
2021-12-01 01:26:54 +0000

    Deploying to gh-pages from @ rc23/journey@8d84d532d060f8900f6d1daf8ed028e1f54f0935 🚀

--- _next/static/5i1KANBg2amo7YDL0UFr4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-83c22936694484ce.js"],"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],"/about":["static/chunks/pages/about-815bf8fae74217d2.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
---
+++ _next/static/c54zhwW3YbgKhx5BNAxq5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0bc97ec24b7b894d.js"],"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],"/about":["static/chunks/pages/about-815bf8fae74217d2.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
