24558805bfad0c6442a928b046d55eac49af9e48
rc23 <kumipt23@gmail.com>
2021-12-01 01:23:21 +0000

    Deploying to gh-pages from @ rc23/journey@66849d706ea2e30128e7094b16e3713d6bb5a9cf 🚀

---
+++ _next/static/5i1KANBg2amo7YDL0UFr4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-83c22936694484ce.js"],"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],"/about":["static/chunks/pages/about-815bf8fae74217d2.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
