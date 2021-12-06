1addb9b66aa504cec62c6aad97268874074e7d26
rc23 <kumipt23@gmail.com>
2021-12-06 23:51:27 +0000

    Deploying to gh-pages from @ rc23/next-js-boilerplate@4451c45417b266d0275a65d29686d8d05a0579a5 🚀

--- _next/static/AuGnYD1RIRhVe0RpCYXup/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-adbb16504cc0cd56.js"],"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],"/about":["static/chunks/pages/about-815bf8fae74217d2.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
